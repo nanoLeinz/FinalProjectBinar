5514bd1ae491fd12e9f0a4f7af9703dc218de5ba
nano <nanoleinz@gmail.com>
2024-01-09 18:09:04 +0700

    Init

---
+++ src/main/java/id/synrgy/travimate/TravimateApplication.java
@@ -0,0 +1,13 @@
+package id.synrgy.travimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/synrgy/travimate/TravimateApplicationTests.java
@@ -0,0 +1,13 @@
+package id.synrgy.travimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
